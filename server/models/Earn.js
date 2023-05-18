05ad6c48b4414ddd6642cf1e5520734417cb6ddd
chandan-shrivastava <chandanshrivastava2002@gmail.com>
2023-05-18 14:47:37 +0530

    Backend added

---
+++ server/models/Earn.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const EarnSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    amount: {
+        type: Number,
+    },
+    rate: {
+        type: Number,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    duration: {
+        type: Number,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('Earn', EarnSchema);
